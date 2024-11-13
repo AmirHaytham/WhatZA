d4194958044d2c6f2c80d4a1b14fcdb420a7e344
AMIR-GIU <amir.haytham@giu-uni.de>
2024-11-13 20:00:40 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WhatsAppLoginPage from './WhatsAppLoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <WhatsAppLoginPage />
+    </div>
+  );
+}
+
+export default App;
